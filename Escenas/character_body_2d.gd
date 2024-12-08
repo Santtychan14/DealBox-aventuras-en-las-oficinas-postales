9a123bfc934d8afd12326d1a47e7f6b0786c661f
SantiagoInfante <santiagoinfante2010@gmail.com>
2024-12-08 13:05:51 -0400

    Algunos bugs y enemigo

---
+++ Escenas/character_body_2d.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+# Hace esto oink xd
--- Scritps/jump_pad.gd
+++ Scritps/jump_pad.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		body.velocity.y = -340
+		body.velocity.y = -350
 		$AnimatedSprite2D.play("Jump pad activado")
