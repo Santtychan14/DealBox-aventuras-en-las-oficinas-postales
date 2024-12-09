f3251761acb8d00938e72ae3d4ab96234ba189a0
Oink55 <santidarago@gmail.com>
2024-12-09 13:39:48 -0300

    Enemigo, fondo y vidas

--- Escenas/character_body_2d.gd
+++ Escenas/character_body_2d.gd
@@ -1,3 +1,29 @@
 extends Area2D
 
+var going_right = true
+var speed = 30
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
 # Hace esto oink xd
+func _physics_process(delta: float) -> void:
+	
+	if going_right == true:
+		position.x += speed * delta
+	else:
+		position.x -= speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.life -= 1
+		body.lose_hearth()
+	speed = 0
+	await get_tree().create_timer(0.5).timeout
+	speed = 30
+	if going_right == true:
+		sprite_2d.flip_h = true
+		going_right = false
+	else:
+		sprite_2d.flip_h = false
+		going_right = true
--- Scritps/player.gd
+++ Scritps/player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 160.0
 const JUMP_VELOCITY = -260.0
+var life = 3
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
+	if life <= 0:
+		get_tree().reload_current_scene()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,5 +25,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	if direction < 0:
+		animated_sprite_2d.flip_h = true
 	move_and_slide()
+
+func lose_hearth():
+	$"../UI/hearths".size.x -= 14
+	
