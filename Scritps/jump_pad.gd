6855caa30f3e5b2d7e5584e13b7bbb03ddc93cba
Oink55 <santidarago@gmail.com>
2024-12-10 19:11:54 -0300

    Sonidos y efectos de daño

--- Scritps/jump_pad.gd
+++ Scritps/jump_pad.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+@onready var platform_jump: AudioStreamPlayer = $platform_jump
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.velocity.y = -350
 		$AnimatedSprite2D.play("Jump pad activado")
+		platform_jump.play()
--- Scritps/player.gd
+++ Scritps/player.gd
@@ -2,11 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 160.0
-const JUMP_VELOCITY = -260.0
 var life = 3
 var puntuaje = 0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var shake_timer: Timer = $Camera2D/shake_timer
+@onready var life_vignette: TextureRect = $"../UI/life_vignette"
 
+#Sonidos
+@onready var pickup: AudioStreamPlayer = $pickup
+@onready var damage: AudioStreamPlayer = $damage
+
+
+
+func _process(delta: float) -> void:
+	if not shake_timer.is_stopped():
+		camera_2d.offset.x = randf_range(0.1,2)
+		camera_2d.offset.y = randf_range(0.1,2)
+		
 
 func _physics_process(delta: float) -> void:
 	if life <= 0:
@@ -16,9 +29,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -34,16 +44,23 @@ func _physics_process(delta: float) -> void:
 
 func lose_hearth():
 	$"../UI/hearths".size.x -= 14
+	camera_shake()
+	damage_vignette()
+	damage.play()
 	
 func puntos():
 	puntuaje += 1
 	$"../UI/Puntos".text = str(puntuaje)
+	pickup.play()
 	
+# voy a a hacer que cuando toque el enemigo se mueva un poco la camara, para mas feedback
+func camera_shake():
+	shake_timer.start()
 
-
-	
-	
-	
+func damage_vignette():
+	var tween = get_tree().create_tween()
+	tween.tween_property(life_vignette,"modulate",Color(1,1,1,0.25),0.15)
+	tween.tween_property(life_vignette,"modulate",Color(1,1,1,0),0.25)
 
 
 	
